8816ab707c4b37573a1221a4a70ad87011878323
yoonmyunghoon <youn1791472@gmail.com>
2021-02-01 21:17:29 +0900

    Entity 추가

---
+++ src/main/java/com/cos/blog/model/Board.java
@@ -0,0 +1,59 @@
+package com.cos.blog.model;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Board {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false, length = 100)
+	private String title;
+	
+	@Lob // 대용량 데이터
+	private String content; // 섬머노트 라이브러리를 사용할 예정, html 태그가 섞여서 디자인이 되기 때문에 용량이 클 것으로 예상됨 
+	
+	@ColumnDefault("0")
+	private int count; // 조회수
+	
+	// ManyToOne은 fetch = FetchType.EAGER가 Default
+	@ManyToOne(fetch = FetchType.EAGER) // Many = Board, One = User
+	@JoinColumn(name="userId")
+	private User user; // DB는 오브젝트를 저장할 수 없음(FK를 사용해야함) - 자바는 오브젝트를 저장할 수 있음 -> 충돌이 발생하게 됨 -> JPA가 해결
+	
+	// OneToMany은 fetch = FetchType.LAZY가 Default, 여기서는 SELECT했을 때 바로 받아올 수 있도록 EAGER 사용
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // mappedBy가 있으면 연관관계의 주인이 아님을 의미(FK가 아니므로 DB에 컬럼을 만들지 말라는 의미)
+	private List<Reply> reply;
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+	
+
+}
---
+++ src/main/java/com/cos/blog/model/User.java
@@ -0,0 +1,50 @@
+package com.cos.blog.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+//ORM -> Object를 테이블로 매핑해주는 기술
+@Entity //User Class가 자동으로 MySQL에 테이블로 생성됨
+public class User {
+	
+	@Id // Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //프로젝트에서 연결된 DB의 넘버링 전략을 따라감
+	private int id; // 시퀀스(오라클), auto-increment(MySQL)
+	
+	@Column(nullable = false, length = 30)
+	private String username; // ID
+	
+	@Column(nullable = false, length = 100)
+	private String password;
+	
+	@Column(nullable = false, length = 50)
+	private String Email;
+	
+	@ColumnDefault("'user'")
+	private String role; // Enum을 쓰는게 좋음 > admin or user or manager 등 해당 컬럼에 도메인을 줄 수 있음
+	
+	@CreationTimestamp //시간 자동 입력
+	private Timestamp createDate;
+	
+	
+	
+
+}
