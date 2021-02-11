ba1c1f2f5b242c4a9f16b19795f7250c9631fb46
yoonmyunghoon <youn1791472@gmail.com>
2021-02-11 22:45:12 +0900

    게시글 수정 기능 추가

--- src/main/java/com/cos/blog/contoller/api/BoardApiController.java
+++ src/main/java/com/cos/blog/contoller/api/BoardApiController.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +32,14 @@ public class BoardApiController {
 	public ResponseDto<Integer> deleteById(@PathVariable int id, @AuthenticationPrincipal PrincipalDetail principal) {
 		boardService.글삭제하기(id, principal.getUser());
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+	}
+	
+	@PutMapping("/api/board/{id}")
+	public ResponseDto<Integer> update(@PathVariable int id, @RequestBody Board board, @AuthenticationPrincipal PrincipalDetail principal) {
+		boardService.글수정하기(id, board, principal.getUser());
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+		
+		
 	}
 
 }
--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -44,13 +44,28 @@ public class BoardService {
 	@Transactional
 	public void 글삭제하기(int id, User user) {
 		Board board = boardRepository.findById(id).orElseThrow(()->{
-			return new IllegalArgumentException("글 찾기 실패 : 해당 글이 존재하지 않습니다.");
+			return new IllegalArgumentException("글 삭제 실패 : 글 아이디를 찾을 수 없습니다. id : "+id);
 		});
 		
 		if (board.getUser().getId() != user.getId()) {
 			throw new IllegalArgumentException("글 삭제 실패 : 해당 글을 삭제할 권한이 없습니다.");
 		}
 		boardRepository.deleteById(id);
+	}
+	
+	@Transactional
+	public void 글수정하기(int id, Board requestBoard, User user) {
+		Board board = boardRepository.findById(id)
+				.orElseThrow(()->{
+					return new IllegalArgumentException("글 수정 실패 : 글 아이디를 찾을 수 없습니다. id : "+id);
+				}); // 영속화 완료 
+		
+		if (board.getUser().getId() != user.getId()) {
+			throw new IllegalArgumentException("글 수정 실패 : 해당 글을 수정할 권한이 없습니다.");
+		}
+		board.setTitle(requestBoard.getTitle());
+		board.setContent(requestBoard.getContent());
+		// 해당 함수 종료시(Service가 종료 될 때) 트랜잭션이 종료됨, 이때 더티체킹 -> 자동 업데이트가 됨(db flush)
 	}
 
 }
